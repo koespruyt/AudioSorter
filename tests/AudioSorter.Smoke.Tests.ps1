ef10128e390304b6f01965d83223110032483f62
koespruyt <koe.spruyt@gmail.com>
2026-02-22 18:58:45 +0100

    ci: fix Pester workflow + add smoke tests

--- tests/AudioSorter.Smoke.Tests.ps1
+++ tests/AudioSorter.Smoke.Tests.ps1
@@ -2,7 +2,6 @@ Describe "AudioSorter smoke" {
   It "Module manifest exists" {
     Test-Path (Join-Path $PSScriptRoot "..\src\AudioSorter\AudioSorter.psd1") | Should -BeTrue
   }
-
   It "Module imports and exports Invoke-AudioSorter" {
     Import-Module (Join-Path $PSScriptRoot "..\src\AudioSorter\AudioSorter.psd1") -Force
     (Get-Command Invoke-AudioSorter -ErrorAction Stop).CommandType | Should -Be 'Function'
