22fcdc23f363ee0c65f168ef28d8c73ecc604673
koespruyt <koe.spruyt@gmail.com>
2026-02-22 16:00:05 +0100

    Delete tests directory

--- tests/AudioSorter.Tests.ps1
+++
@@ -1,6 +0,0 @@
-Describe 'AudioSorter' {
-  It 'Module imports' {
-    $mod = Join-Path $PSScriptRoot '..\src\AudioSorter\AudioSorter.psd1'
-    { Import-Module $mod -Force } | Should -Not -Throw
-  }
-}
